1904fdd527a748f8487fc701dac4fcd3e28077e8
Yerko Palma <yerko.palma@usach.cl>
2020-02-11 12:20:52 -0300

    most basic implementation

---
+++ README.md
@@ -0,0 +1,57 @@
+# @yerkopalma/cursor
+
+Custom cursor component
+
+## Usage
+
+```svelte
+<script>
+  import Cursor from '@yerkopalma/cursor'
+</script>
+
+<Cursor />
+```
+
+## Install
+
+```bash
+npm install --save-dev @yerkopalma/cursor
+```
+
+## API
+
+### `Cursor` component
+
+The default export is the svelte component adding it into your 
+page will affect the your whole site. You can also set the following properties:
+
+- `global: boolean = true` => Set to false to change the cursor element only on the parent component.
+- `type: ['dot', 'circle', 'cross', 'minus']` => Predefined types of cursor, defaults to dot.
+
+### actions
+
+If you want a different cursor on hover for specific elements, you should 
+use predefined actions on those elements, example:
+
+```svelte
+<script>
+import { magneticCursor } from '@yerkopalma/cursor'
+</script>
+
+<a use:magneticCursor>magnetic</a>
+```
+
+Availaible actions are:
+
+- magneticCursor
+- dotCursor
+- crossCursor
+- minusCursor
+- borderCursor
+
+Also, you can define custom actions for custom hovers
+
+TODO: Document custom actions
+
+## License
+MIT
--- src/components/Cursor.svelte
+++ src/components/Cursor.svelte
@@ -1,11 +1,89 @@
 <script>
-  export let name = 'stranger';
+  import { spring } from 'svelte/motion'
+
+  let coords = spring({ x: -100, y: -100 }, {
+    stiffness: 0.1,
+    damping: 0.25
+  })
+  const handleMouseMove = (e) => coords.set({ x: e.clientX, y: e.clientY })
 </script>
 
-<style>
-  h1 {
-    color: purple;
-  }
-</style>
+<svelte:body
+  on:mousemove={handleMouseMove}
+/>
 
-<h1>Hello {name}!</h1>
+<div class="cursor-wrapper">
+  <div class="custom-cursor custom-cursor__outer" style="transform: translate({$coords.x}px, {$coords.y}px)">
+    <div class="custom-cursor custom-cursor__inner"></div>
+  </div>
+</div>
+
+<style type="text/scss">
+:global(body) {
+  cursor: none !important;
+  min-height: 100vh;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+}
+.cursor-wrapper {
+  position: fixed;
+  opacity: 1;
+  width: 100px;
+  height: 100px;
+  left: -50px;
+  top: -50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 1600000;
+  pointer-events: none;
+  &.has-blend-mode {
+    mix-blend-mode: exclusion;
+  }
+}
+
+.custom-cursor {
+  position: relative;
+  left: 0;
+  top: 0;
+  filter: invert(100%);
+
+  &__outer {
+    width: 6px;
+    height: 6px;
+    border: 2px solid white;
+    background: white;
+    border-radius: 50%;
+  }
+
+  &__inner {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    &:before,
+    &:after {
+      content: "";
+      position: absolute;
+      width: 40%;
+      height: 1px;
+      background: white;
+      transition: transform 0.1s linear;
+    }
+    &:before {
+      left: 50%;
+      top: 50%;
+      transform: translateX(-50%);
+    }
+    &:after {
+      left: 50%;
+      top: 50%;
+      transform: translateX(-50%) rotate(-90deg);
+    }
+    &.is-closing:after {
+      transform: translateX(-50%) rotate(0deg);
+    }
+  }
+}
+
+</style>
